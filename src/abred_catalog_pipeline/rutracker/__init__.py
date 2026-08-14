ada90627bcb49116b391587b76a459bfcbfeb645
ivzaislu <johann0789@googlemail.com>
2026-08-14 15:00:36 +0200

    RuTracker: подключить безопасный cover selector

--- src/abred_catalog_pipeline/rutracker/__init__.py
+++ src/abred_catalog_pipeline/rutracker/__init__.py
@@ -1,8 +1,10 @@
 from . import parser as _parser
+from .cover import select_cover_from_post
 from .retry_client import RetryingRuTrackerWorkerClient
 
 DEFAULT_AUDIOBOOK_FORUM_IDS = _parser.DEFAULT_AUDIOBOOK_FORUM_IDS
 _parser.RuTrackerWorkerClient = RetryingRuTrackerWorkerClient
+_parser._cover_from_post = select_cover_from_post
 RuTrackerWorkerClient = RetryingRuTrackerWorkerClient
 
 __all__ = ["DEFAULT_AUDIOBOOK_FORUM_IDS", "RuTrackerWorkerClient"]
