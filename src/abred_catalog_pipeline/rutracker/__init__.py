c227ac1d8aa7265082f11d6341b9bab34843bda6
ivzaislu <johann0789@googlemail.com>
2026-08-14 15:07:53 +0200

    RuTracker: подключить безопасную нормализацию series_name

--- src/abred_catalog_pipeline/rutracker/__init__.py
+++ src/abred_catalog_pipeline/rutracker/__init__.py
@@ -1,10 +1,12 @@
 from . import parser as _parser
 from .cover import select_cover_from_post
 from .retry_client import RetryingRuTrackerWorkerClient
+from .series import normalize_series_name
 
 DEFAULT_AUDIOBOOK_FORUM_IDS = _parser.DEFAULT_AUDIOBOOK_FORUM_IDS
 _parser.RuTrackerWorkerClient = RetryingRuTrackerWorkerClient
 _parser._cover_from_post = select_cover_from_post
+_parser._normalize_series_name = normalize_series_name
 RuTrackerWorkerClient = RetryingRuTrackerWorkerClient
 
 __all__ = ["DEFAULT_AUDIOBOOK_FORUM_IDS", "RuTrackerWorkerClient"]
