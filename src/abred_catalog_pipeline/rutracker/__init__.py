ed0cf0a963eb5d87b7e91cf6f9b233d81adb4ee1
ivzaislu <johann0789@googlemail.com>
2026-08-22 01:03:02 +0200

    Enable nested RuTracker metadata boundary guard

--- src/abred_catalog_pipeline/rutracker/__init__.py
+++ src/abred_catalog_pipeline/rutracker/__init__.py
@@ -1,5 +1,6 @@
 from . import parser as _parser
 from .cover import select_cover_from_post
+from .field_boundary import post_field_with_nested_label_guard
 from .retry_client import RetryingRuTrackerWorkerClient
 from .series import normalize_series_name
 
@@ -7,6 +8,7 @@ DEFAULT_AUDIOBOOK_FORUM_IDS = _parser.DEFAULT_AUDIOBOOK_FORUM_IDS
 _parser.RuTrackerWorkerClient = RetryingRuTrackerWorkerClient
 _parser._cover_from_post = select_cover_from_post
 _parser._normalize_series_name = normalize_series_name
+_parser._post_field = post_field_with_nested_label_guard
 RuTrackerWorkerClient = RetryingRuTrackerWorkerClient
 
 __all__ = ["DEFAULT_AUDIOBOOK_FORUM_IDS", "RuTrackerWorkerClient"]
